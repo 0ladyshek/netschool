57d1f6b70f16a3a05a349d6ea65ede656e5fd2e7
0ladyshek <62494258+0ladyshek@users.noreply.github.com>
2022-01-31 16:29:22 +0300

    Delete pluginsvk directory

--- pluginsvk/dnevnik.py
+++
@@ -1,8 +0,0 @@
-from vkbottle.bot import Blueprint, Message
-
-bp = Blueprint('dnevnik')
-bp.labeler.ignore_case = True
-
-@bp.on.chat_message(text="/dnevnik")
-async def _(message: Message):
-    await message.answer("")
--- pluginsvk/info.py
+++
@@ -1,6 +0,0 @@
-from vkbottle.bot import Blueprint
-
-bp = Blueprint('info')
-
-async def info(event):
-	await bp.api.messages.send(event.object.user_id, 0, message='🔶Создатель: [id350673924|Ёж]\n\n🔸Бот в ТГ: t.me/netschoolbot\n🔹Бот в Дискорде: <в разработке>')
